c16f81004ad8defb3d10cc48bb5880908850a98c
MarkKapkan <mark.kapkan@gmail.com>
2025-08-25 19:55:45 +0300

    Created Nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,22 @@
 import React from "react";
-
+import "./Nav.css"
+import navLogo from "./nav-logo.svg"
+import { VscAccount } from "react-icons/vsc";
 const Nav = () => {
   return (
     <nav className="Nav">
-      
+      <div className="Logo-section">
+        <img src={navLogo}></img>
+      </div>
+      <div className="Central-section">
+        <a>Who we are</a> <br />
+        <a>Contacts</a> <br />
+        <a>Menu</a> <br />
+      </div>
+      <div className="Account-section">
+        <button className="Signup-button">Sign Up</button>
+        <VscAccount className="Account-icon"/>
+      </div>
     </nav>
   );
 };
